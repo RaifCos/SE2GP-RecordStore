7802ef0d44d5381e19ec4640a0bcc76bb0829bb9
RaifCos <128611497+RaifCos@users.noreply.github.com>
2024-10-31 11:32:25 +0000

    Added Cart Entity to Backend
    but it works this time

---
+++ records/build/classes/java/main/com/ct5106/records/Cart.java
@@ -0,0 +1,98 @@
+package com.ct5106.records.domain;
+
+import jakarta.persistence.*;
+import java.util.List;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+// TODO: Refactor to change List<Record> into List<Product>.
+
+@Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+public class Cart {
+
+    // Generate Cart ID
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long cartID;
+
+    // Establish Relationship between Cart and AppUser.
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name="id")
+    private AppUser user;
+
+    // Establish Relationship between Cart and Record.
+    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Record> records;
+
+    private double total;
+
+    /**
+     * Constructor
+     * @param user, to be assigned to user.
+     * @param records, to be assigned to user.
+     */
+    public Cart(AppUser user, List<Record> records) {
+        this.user = user;
+        this.records = records;
+    }
+
+    /**
+     * Empty Constructor
+     */
+    public Cart() {
+
+    }
+
+    /**
+     * Getter Method for cartID.
+     * @return cartID.
+     */
+    public long getID() { return cartID; }
+
+    /**
+     * Setter Method for cartID.
+     * @param cartID, to be assigned to cartID.
+     */
+    public void setID(long cartID) { this.cartID = cartID; }
+
+    /**
+     * Getter Method for user.
+     * @return user.
+     */
+    public AppUser getUser() { return user; }
+
+    /**
+     * Setter Method for user
+     * @param user, to be assigned to user.
+     */
+    public void setUser(AppUser user) { this.user = user; }
+
+    /**
+     * Getter Method for records.
+     * @return records.
+     */
+    public List<Record> getRecords() { return records; }
+
+    /**
+     * Setter Method for records
+     * @param records, to be assigned to records.
+     */
+    public void setRecords(List<Record> records) { this.records = records; }
+
+    /**
+     * Getter Method for total.
+     * @return total.
+     */
+    public double getTotal() { return total; }
+
+    /**
+     * Setter Method for Total (No Parameters as Total is defined by records).
+     */
+    public void setTotal() {
+        total = 0.0f;
+        // Get the Sum of each Price in the Cart.
+        for(Record r : records) { total += r.getPrice(); }
+        // Round to 2 Decimal Places
+        total = (Math.round(total * 100f)) / 100f;
+    }
+}
