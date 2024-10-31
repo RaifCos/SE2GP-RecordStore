e77aa16633aa6ec4dd2a6c1a1a7043cde66f7390
RaifCos <128611497+RaifCos@users.noreply.github.com>
2024-10-31 12:03:41 +0000

    Update App.jsx
    Added Cart Entity to Frontend (YIPPEE!!)

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css';
 import RecordsTableComponent from './RecordsTableComponent';
 import ArtistsTableComponent from './ArtistsTableComponent';
+import CartsTableComponent from './CartsTableComponent';
 
 function App() {
   return (
     <div className="App">
       <RecordsTableComponent />
       <ArtistsTableComponent />
+      <CartsTableComponent />
     </div>
   );
 }
