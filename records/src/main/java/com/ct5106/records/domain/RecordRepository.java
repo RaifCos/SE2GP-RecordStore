848b4c752ac25d6a109294320297aa667d699145
Mira <150616182+poppybun@users.noreply.github.com>
2024-11-01 18:31:42 +0000

    Update RecordRepository.java
    removed redundant code

--- records/src/main/java/com/ct5106/records/domain/RecordRepository.java
+++ records/src/main/java/com/ct5106/records/domain/RecordRepository.java
@@ -5,28 +5,13 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-
 import java.util.List;
+import java.util.Optional;
 
 @RepositoryRestResource
 public interface RecordRepository extends CrudRepository<Record, Long> {
-    @Query("select r from Record r join r.artist a where a.name = :artistName")
-    List<Record> findByArtistName(@Param("artistName") String artistName);
-
-    @Query("select r from Record r where r.name = :name")
-    List<Record> findByRecordName(@Param("name") String name);
-
-
-    @Query("select r from Record r where r.genre = :genre")
-    List<Record> findByGenre(@Param("genre") String genre);
-
-
-    @Query("select r from Record r where r.yearReleased = :yearReleased")
-    List<Record> findByYearReleased(@Param("yearReleased") int yearReleased);
-
-    @Query("select r from Record r where r.id = :id")
-    List<Record> findByRecordId(@Param("id") Long id);
-
-    @Query("select c from Record c where c.price between ?1 and ?2")
-    List<Record> findByPriceBetween(@Param("low") double low, @Param("high") double high);
+    @Override
+    Optional<Record> findById(Long id);
+    @Override
+    List<Record> findAll();
 }
