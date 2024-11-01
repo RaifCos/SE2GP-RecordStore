d025d156d9972262b424d6f1491f6fef20b07446
Mira <150616182+poppybun@users.noreply.github.com>
2024-11-01 18:29:39 +0000

    Update CartRepository.java
    removed redundant code

--- records/src/main/java/com/ct5106/records/domain/CartRepository.java
+++ records/src/main/java/com/ct5106/records/domain/CartRepository.java
@@ -1,13 +1,9 @@
 package com.ct5106.records.domain;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-@RepositoryRestResource
-public interface CartRepository extends CrudRepository<Cart, Long> {
-    @Query("select c from Cart c where c.user = :user")
-    List<Cart> findByName(@Param("user") AppUser user);
+public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+    List<CartItem> findAll();
 }
