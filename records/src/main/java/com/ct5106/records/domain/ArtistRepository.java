5f5caeca12880b3cd980bf7129d4932adbc99561
Mira <150616182+poppybun@users.noreply.github.com>
2024-11-01 18:26:35 +0000

    Update ArtistRepository.java
    removed queries

--- records/src/main/java/com/ct5106/records/domain/ArtistRepository.java
+++ records/src/main/java/com/ct5106/records/domain/ArtistRepository.java
@@ -1,19 +1,14 @@
 package com.ct5106.records.domain;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
+
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 
 @RepositoryRestResource
-public interface ArtistRepository extends CrudRepository<Artist, Long> {
-    @Query("select a from Artist a where a.name = :name")
-    List<Artist> findByName(@Param("name") String name);
+public interface ArtistRepository extends JpaRepository<Artist, Long> {
 
-    @Query("select a from Artist a where a.numMembers = :numMembers")
-    List<Artist> findByNumMembers(@Param("numMembers") int numMembers);
-
-    @Query("select c from Artist c where c.numMembers between ?1 and ?2")
-    List<Artist> findByMembersBetween(@Param("low") double low, @Param("high") double high);
+    // Method to find all artists
+    @Override
+    List<Artist> findAll();
 }
